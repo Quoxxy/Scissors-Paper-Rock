1f496376297ddd565199ee4e32208778c25b35f4
Quoxxy <91362932+Quoxxy@users.noreply.github.com>
2023-02-28 11:45:28 +1100

    Created if/if else function
    if else function, which puts playerSelection against computerSelection in order to decide the rounds outcome

--- RPS.js
+++ RPS.js
@@ -1,7 +1,30 @@
-const options = ["Rock", "Paper", "Scissors"];
+const options = ["rock", "paper", "scissors"];
 
 function getComputerSelection(){
-const random = [Math.floor(Math.random() * options.length)];
-const ComputerSelection = options[random];
-console.log(ComputerSelection)
+  const random = [Math.floor(Math.random() * options.length)];
+  const computerChoice = options[random];
+  return computerChoice;
 }
+
+function playRound(playerSelection, computerSelection) {
+  if (playerSelection === computerSelection) {
+    alert("It's a Tie!");
+  } else if (
+    (playerSelection === "rock" && computerSelection === "paper") ||
+    (playerSelection === "paper" && computerSelection === "scissors") ||
+    (playerSelection === "scissors" && computerSelection === "rock")
+  ) {
+    alert("You lose!");
+  } else if (
+    (playerSelection === "paper" && computerSelection === "rock") ||
+    (playerSelection === "rock" && computerSelection === "scissors") ||
+    (playerSelection === "scissors" && computerSelection === "paper")
+  ) {
+    alert("You Win!");
+  }
+}
+
+playRound(playerSelection, computerSelection);
+
+console.log(playRound);
+
